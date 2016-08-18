d51a63314e427331d065bad8d31ee5fda190d510
Mason Chang <mchang@mozilla.com>
2016-08-18 14:51:42 -0700

    basic glutin window working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate gl;
+extern crate glutin;
+extern crate libc;
+
 fn main() {
-  println!("Hello World");
+    let window = glutin::Window::new().unwrap();
+
+    unsafe {
+        window.make_current();
+        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
+    }
+
+    for event in window.wait_events() {
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+        window.swap_buffers();
+
+        match event {
+            glutin::Event::Closed => break,
+            _ => ()
+        }
+    }
 }
