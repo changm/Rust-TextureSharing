6d6ed081002cdf6ec0893110bcd10bef48f07d07
Mason Chang <mchang@mozilla.com>
2016-08-18 17:02:41 -0700

    switch to gleam gl to match webrender. upload vertex data

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-extern crate gl;
 extern crate glutin;
 extern crate libc;
 extern crate image;
+extern crate gleam;
 
 use std::fs::File;
 use std::path::Path;
 
-use gl::types::*;
+use gleam::gl;
 
 // Returns the raw pixel data
 fn get_image_data() -> Vec<u8> {
@@ -17,9 +17,29 @@ fn get_image_data() -> Vec<u8> {
 
 fn upload_texture(texture_data: Vec<u8>) {
     unsafe {
-        let mut texture_id: GLuint = 0;
+        let mut texture_id: gl::GLuint = 0;
         gl::GenBuffers(1, &mut texture_id);
         println!("Generated texture id is: {:?}", texture_id);
+    }
+}
+
+fn upload_triangle() {
+    let vertices: [f32; 6] = [
+        0.0, 0.5,   // V1
+        0.5, -0.5,  // V2
+        -0.5, -0.5  // V3
+    ];
+
+    unsafe {
+        let mut triangle_vbo: gl::GLuint = 0;
+        gl::GenBuffers(1, &mut triangle_vbo);
+        println!("Generated vertex id : {:?}", triangle_vbo);
+
+        // Now let's upload the data
+        gl::BindBuffer(gl::ARRAY_BUFFER, triangle_vbo);
+
+        // Always want a triangle
+        gl::buffer_data(gl::ARRAY_BUFFER, &vertices, gl::STATIC_DRAW);
     }
 }
 
@@ -35,6 +55,7 @@ fn main() {
 
     // Have to do this after we create the window which loads all the symbols.
     upload_texture(texture_data);
+    upload_triangle();
 
     for event in window.wait_events() {
         unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
