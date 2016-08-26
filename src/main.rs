ce72bb5d846a52f49ff060c2d82bb7b64df2cb8c
Mason Chang <mchang@mozilla.com>
2016-08-25 17:18:48 -0700

    move some things into the device object

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,11 @@ pub struct Device {
     quad_vertex_shader : Option<gl::GLuint>,
     quad_fragment_shader : Option<gl::GLuint>,
     pid : gl::GLuint,
+
+    m_vao : gl::GLuint,
+    // indices
+    m_ibo : gl::GLuint,
+    m_vbo : gl::GLuint,
 }
 
 impl Device {
@@ -31,7 +36,10 @@ impl Device {
         let mut device = Device { m_fbo: 0,
                               quad_vertex_shader : Some(0),
                               quad_fragment_shader : Some(0),
-                              pid : 0};
+                              pid : 0,
+                              m_vao : 0,
+                              m_ibo : 0,
+                              m_vbo : 0};
 
         let vertex_shader = String::from("/Users/masonchang/Projects/Rust-TextureSharing/shaders/vertex.glsl");
         let fragment_shader = String::from("/Users/masonchang/Projects/Rust-TextureSharing/shaders/fragment.glsl");
@@ -50,6 +58,65 @@ impl Device {
         gl::use_program(device.pid);
 
         return device;
+    }
+
+    pub fn setup_vao(&mut self) {
+        let vertices: [f32; 16] =
+        [
+            // vertices     // Texture coordinates, origin is bottom left, but images decode top left origin
+            // So we flip our texture coordinates here instead.
+            -1.0, -1.0,     0.0, 1.0,  // Bottom left
+            -1.0, 1.0,      0.0, 0.0, // Top Left
+            1.0, 1.0,       1.0, 0.0,    // Top right
+            1.0, -1.0,      1.0, 1.0,  // bottom right
+        ];
+
+        let indices : [u32 ; 6] =
+        [
+            0, 1, 2, // Actually have to connect the whole screen
+            2, 3, 0,
+        ];
+
+        // VAOs have to be genreated fairly early then.
+        let vaos = gl::gen_vertex_arrays(1);
+        self.m_vao = vaos[0];
+        gl::bind_vertex_array(self.m_vao);
+
+        // generate our FBO
+        let fbos = gl::gen_framebuffers(1);
+        self.m_fbo = fbos[0];
+        gl::bind_framebuffer(gl::FRAMEBUFFER, self.m_fbo);
+
+        // Buffers for our index array
+        let ibo_buffers = gl::gen_buffers(1);
+        self.m_ibo = ibo_buffers[0];
+
+        // Upload vertex data
+        let vbos = gl::gen_buffers(1);
+        self.m_vbo = vbos[0];
+        gl::bind_buffer(gl::ARRAY_BUFFER, self.m_vbo);
+        gl::buffer_data(gl::ARRAY_BUFFER, &vertices, gl::STATIC_DRAW);
+
+        // Upload our index data
+        gl::bind_buffer(gl::ELEMENT_ARRAY_BUFFER, self.m_ibo);
+        gl::buffer_data(gl::ELEMENT_ARRAY_BUFFER, &indices, gl::STATIC_DRAW);
+
+        // Now make the link between vertex data and attributes
+        let vertex_count = 2;
+        let vertex_stride = (mem::size_of::<f32>() * 4) as i32;
+        let f32_size = mem::size_of::<f32>();
+        let pos_attribute = gl::get_attrib_location(self.pid, "position");
+        gl::enable_vertex_attrib_array(pos_attribute as u32);
+        gl::vertex_attrib_pointer(pos_attribute as u32, vertex_count, gl::FLOAT, false, vertex_stride, 0);
+
+        let tex_attribute = gl::get_attrib_location(self.pid, "texture_coord");
+        gl::enable_vertex_attrib_array(tex_attribute as u32);
+        gl::vertex_attrib_pointer(tex_attribute as u32,
+                vertex_count,
+                gl::FLOAT,
+                false,
+                vertex_stride,
+                (f32_size * 2) as u32);
     }
 
     pub fn get_program_id(&self) -> gl::GLuint {
@@ -96,21 +163,7 @@ fn upload_texture(width: u32, height: u32, data: &[u8]) {
     ];
 
     let mut device = Device::new();
-    let pid = device.get_program_id();
-
-        // VAOs have to be genreated fairly early then.
-    let vaos = gl::gen_vertex_arrays(1);
-    let vao = vaos[0];
-    gl::bind_vertex_array(vao);
-
-    // Let's render to an FBO instead
-    let fbos = gl::gen_framebuffers(1);
-    let fbo = fbos[0];
-    gl::bind_framebuffer(gl::FRAMEBUFFER, fbo);
-
-    // Buffers for our index array
-    let ibo_buffers = gl::gen_buffers(1);
-    let ibo_buffer = ibo_buffers[0];
+    device.setup_vao();
 
     // Buffers for our textures
     let texture_buffers = gl::gen_textures(1);
@@ -135,39 +188,12 @@ fn upload_texture(width: u32, height: u32, data: &[u8]) {
                      gl::UNSIGNED_BYTE,
                      Some(data));
 
-    // Upload vertex data
-    let vbos = gl::gen_buffers(1);
-    let image_vbo = vbos[0];
-    gl::bind_buffer(gl::ARRAY_BUFFER, image_vbo);
-    gl::buffer_data(gl::ARRAY_BUFFER, &vertices, gl::STATIC_DRAW);
-
-    // Upload our index data
-    gl::bind_buffer(gl::ELEMENT_ARRAY_BUFFER, ibo_buffer);
-    gl::buffer_data(gl::ELEMENT_ARRAY_BUFFER, &indices, gl::STATIC_DRAW);
-
-    // Now make the link between vertex data and attributes
-    let vertex_count = 2;
-    let vertex_stride = (mem::size_of::<f32>() * 4) as i32;
-    let f32_size = mem::size_of::<f32>();
-    let pos_attribute = gl::get_attrib_location(pid, "position");
-    gl::enable_vertex_attrib_array(pos_attribute as u32);
-    gl::vertex_attrib_pointer(pos_attribute as u32, vertex_count, gl::FLOAT, false, vertex_stride, 0);
-
-    let tex_attribute = gl::get_attrib_location(pid, "texture_coord");
-    gl::enable_vertex_attrib_array(tex_attribute as u32);
-    gl::vertex_attrib_pointer(tex_attribute as u32,
-                              vertex_count,
-                              gl::FLOAT,
-                              false,
-                              vertex_stride,
-                              (f32_size * 2) as u32);
-
-    // What is a VAO for again, it just remembers everything we did here?
+        // What is a VAO for again, it just remembers everything we did here?
     //gl::draw_arrays(gl::TRIANGLES, 0, 6);
     gl::draw_elements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0);
 
     // Lets just copy teh blit
-    gl::bind_framebuffer(gl::READ_FRAMEBUFFER, fbo);
+    gl::bind_framebuffer(gl::READ_FRAMEBUFFER, device.m_fbo);
     gl::bind_framebuffer(gl::DRAW_FRAMEBUFFER, 0);
 
 /*
